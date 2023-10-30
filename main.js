3a9bc1db6999a5b376a705dbe32a450f45598e3b
Andrew Brown <drewkyle01@gmail.com>
2023-10-30 14:24:45 -0400

    Trivia Question in main.js

---
+++ main.js
@@ -0,0 +1,99 @@
+const questions = [
+  {
+    question: "Who was the first president of the United States?",
+    answers: {
+      A: "George Washington", // correct answer
+      B: "Donald Trump",
+      C: "John Adams",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question:
+      "In which year did Christopher Columbus first arrive in the Americas?",
+    answers: {
+      A: "1492", // correct answer
+      B: "1620",
+      C: "1776",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question:
+      "What ancient wonder was located in Alexandria, Egypt, and was one of the Seven Wonders of the Ancient World?",
+    answers: {
+      A: "The Colossus of Rhodes",
+      B: "The Great Wall of China",
+      C: "The Lighthouse of Alexandria", // correct answer
+    },
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "Which battle, fought in 1066, resulted in William the Conqueror becoming the King of England?",
+    answers: {
+      A: "The Battle of Hastings", // correct answer
+      B: "The Battle of Agincourt",
+      C: "The Battle of Bannockburn",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question:
+      "Who was the Egyptian queen known for her relationship with Roman leaders Julius Caesar and Mark Antony?",
+    answers: {
+      A: "Cleopatra", // correct answer
+      B: "Nefertiti",
+      C: "Hatshepsut",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question:
+      "What infamous ship sank on its maiden voyage in 1912, leading to one of the deadliest commercial peacetime maritime disasters in modern history?",
+    answers: {
+      A: "The Millennium Falcon",
+      B: "The Titanic", // correct answer
+      C: "SS Andrea Doria",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "The signing of the Magna Carta in 1215 is considered a significant event in the history of which country?",
+    answers: {
+      A: "France",
+      B: "England", // correct answer
+      C: "Spain",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "Who was the first woman to fly solo across the Atlantic Ocean?",
+    answers: {
+      A: "Amelia Earhart", // correct answer
+      B: "Bessie Coleman",
+      C: "Harriet Quimby",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question: "During which war was the Battle of Gettysburg fought?",
+    answers: {
+      A: "World War II",
+      B: "American Civil War", // correct answer
+      C: "Korean War",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "What was the code name for the Allied invasion of Normandy during World War II?",
+    answers: {
+      A: "Operation Market Garden", // correct answer
+      B: "Operation Barbarossa",
+      C: "Operation Overlord",
+    },
+    correctAnswer: "A",
+  },
+];
